409fbda529aae8ebab2aa86841a60ee219e9d8c1
Mariano Ramirez <marianoramirez353@gmail.com>
2025-07-25 10:24:29 -0400

    learning

--- rust/ownership/src/main.rs
+++ rust/ownership/src/main.rs
@@ -16,4 +16,14 @@ fn main() {
 
     let result = longest(string1.as_str(), string2);
     println!("The longest string is: {}", result);
+
+    let mut vector = vec![0,1,2,2,3,0,4,2];
+    remove_element(&mut vector, 2);
+    println!("The vector is: {:?}", vector);   
+}
+
+fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+    let initial_lenth = nums.len();
+    nums.retain(|&x| x != val);
+    (initial_lenth - nums.len()) as i32
 }
